209ad800f62be540703e54fcbcef9f9f6d747c42
小林克暉 <kobayashikatsuakira@mbp.local>
2020-08-04 17:37:28 +0900

    リファクタリング

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -13,6 +13,10 @@ class LineBotController < ApplicationController
     events = client.parse_events_from(body)
     events.each do |event|
 
+      #説明内容を格納
+      explanation = "下記から選択し、送信ください！\n\n①今日の予定を知りたい\n⇒今日の予定\n②予定を追加したい\n⇒予定追加"
+      #予定追加時の送信内容
+      add_plan = "下記に倣って、送信ください！\n\n'追加'と記載\n日付（半角数字8桁）\n内容"
       #user情報の取得
       user = event['source']['userId']
 
@@ -24,68 +28,29 @@ class LineBotController < ApplicationController
             #全体の使い方
             message = {
               type: 'text',
-              text: "下記から選択し、送信ください！\n\n①今日の予定を知りたい\n⇒今日の予定\n②予定を追加したい\n⇒予定追加"
+              text: explanation
             }
           elsif event.message['text'] == "今日の予定"
-            #今日の予定がリクエスト
-            #今日の予定をDBより抽出
-            today_calendar = Calendar.where(user: user, date: Date.today.strftime('%Y%m%d')).select("content")
 
-            #今日の予定があるか確認
-            if today_calendar.present?
-              #今日の予定を初期化
-              today_plans = ""
-              #該当するユーザーの今日の予定内容を抽出
-              for num in 0..today_calendar.count - 1 do
-                if num == 0
-                  today_plans = "・" + today_calendar[num].content
-                else
-                  today_plans = "#{today_plans}\n・#{today_calendar[num].content}"
-                end
-              end
-
-              #今日の予定を送信
-              message = {
-                type: 'text',
-                text: today_plans
-              }
-            else
-              #今日の予定がない場合
-              message = {
-                type: 'text',
-                text: "今日の予定はなし！"
-              }
-            end
+            #今日の予定を一覧で抽出
+            today_plan_request(user)
 
           elsif event.message['text'] == "予定追加"
             #予定追加する際の、送信内容を送信
             message = {
               type: 'text',
-              text: "下記に倣って、送信ください！\n\n'追加'と記載\n日付（半角数字8桁）\n内容"
+              text: add_plan
             }
 
           elsif event.message['text'].slice(0,2) == "追加"
-            #予定追加のリクエスト
-            date = event.message['text'].slice(3,8)
-            content = event.message['text'].slice(12..)
-            #リクエストされた予定をDBに保存
-            plan = Calendar.new(user: user, date: date, content: content)
-            if plan.save
-              message = {
-                type: 'text',
-                text: "追加しました！前日にリマインドしやす！"
-              }
-            else
-              message = {
-                type: 'text',
-                text: "送信された内容に不備があります。もう一度送信ください。"
-              }
-            end
+
+            #リクエストされた予定を追加
+            add_plan_request(user, event)
 
           else
             message = {
               type: 'text',
-              text: "下記から選択し、送信ください！\n\n①今日の予定を知りたい\n⇒今日の予定\n②予定を追加したい\n⇒予定追加"
+              text: explanation
             }
           end
         end
@@ -95,6 +60,7 @@ class LineBotController < ApplicationController
 
   end
 
+
   private
 
     def client
@@ -102,6 +68,55 @@ class LineBotController < ApplicationController
         config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
         config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
       }
+    end
+
+    def today_plan_request(user)
+      #今日の予定をDBより抽出
+      today_calendar = Calendar.where(user: user, date: Date.today.strftime('%Y%m%d')).select("content")
+
+      #今日の予定があるか確認
+      if today_calendar.present?
+        #今日の予定を初期化
+        today_plans = ""
+        #該当するユーザーの今日の予定内容を抽出
+        for num in 0..today_calendar.count - 1 do
+          if num == 0
+            today_plans = "・" + today_calendar[num].content
+          else
+            today_plans = "#{today_plans}\n・#{today_calendar[num].content}"
+          end
+        end
+
+        #今日の予定を送信
+        message = {
+          type: 'text',
+          text: today_plans
+        }
+      else
+        #今日の予定がない場合
+        message = {
+          type: 'text',
+          text: "今日の予定はなし！"
+        }
+      end
+    end
+
+    def add_plan_request(user, event)
+      date = event.message['text'].slice(3,8)
+      content = event.message['text'].slice(12..)
+      #リクエストされた予定をDBに保存
+      plan = Calendar.new(user: user, date: date, content: content)
+      if plan.save
+        message = {
+          type: 'text',
+          text: "追加しました！前日にリマインドしやす！"
+        }
+      else
+        message = {
+          type: 'text',
+          text: "送信された内容に不備があります。もう一度送信ください。"
+        }
+      end
     end
 
 end
